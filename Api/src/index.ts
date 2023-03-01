fffdb83e7583578e3bf73e7fe8e5362ca2e3a5f1
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-03-01 14:57:50 -0500

    database created and connected

--- Api/src/index.ts
+++ Api/src/index.ts
@@ -1,1 +1,8 @@
-console.log("hola mundo");
+import mongoose from 'mongoose';
+
+async function connectDB () {
+    const db = await mongoose.connect('mongodb://localhost/moveItdb');
+    console.log('database is connected to', db.connection.db.databaseName)
+}
+
+connectDB()
